b986af20493aa4dd6672d72b06096d2597da1ce1
lovelanai <94110810+lovelanai@users.noreply.github.com>
2022-04-12 15:47:45 +0200

    added delete and started with put

--- routers/guitars.router.js
+++ routers/guitars.router.js
@@ -5,28 +5,52 @@ const guitarsRouter = express.Router();
 let guitars = [];
 
 // all routes in here starts with /guitars
-
-guitarsRouter.get("/:id", (req, res) => {
-  const id = req.params;
-  const foundGuitar = guitars.find((guitar) => guitar.id === id);
-  res.send(foundGuitar);
-});
-
 guitarsRouter.get("/", (req, res) => {
   res.send(guitars);
 });
 
+// Adding new Guitar and giving it a random ID-number between 1-100
 guitarsRouter.post("/", (req, res) => {
-  console.log(req.body);
-  guitars.push(req.body);
-  res.status(201).send(`${req.body.brand}, ${req.body.model} added`);
+  const guitar = req.body;
+
+  const guitarId = Math.random().toFixed(2) * 100;
+
+  guitars.push({ ...guitar, id: guitarId });
+
+  res
+    .status(201)
+    .send(`${req.body.brand} ${req.body.model} added with id ${guitarId}`);
+});
+
+// Fetches guitar by id
+guitarsRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundGuitar = guitars.find((guitar) => guitar.id == id);
+  res.status(404).send(`Guitar with ID ${req.params.id} does not exist`);
+  res.send(foundGuitar);
 });
 
 guitarsRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  guitars.find((guitar) => guitar.id == id);
+
   res.send(`guitar with id "${req.params.id}" was updated`);
+  res.status(404).send(
+    `Failed to update Guitar with ID ${req.params.id}
+      try with another id`
+  );
 });
 
 guitarsRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  guitars = guitars.filter((guitar) => guitar.id != id);
+
+  guitars.push(req.body);
+  res.status(404).send(`Failed to delete Guitar with ID ${req.params.id}
+Try again with a different id`);
   res.send(`guitar with id "${req.params.id}" was deleted`);
 });
 
