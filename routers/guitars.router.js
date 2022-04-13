35fb2b7ed8dd18addb5c549c2d3470dddad28add
lovelanai <94110810+lovelanai@users.noreply.github.com>
2022-04-13 20:01:43 +0200

    fixed error messages for all of the CRUD

--- routers/guitars.router.js
+++ routers/guitars.router.js
@@ -1,22 +1,97 @@
 const express = require("express");
+const shortid = require("shortid");
+const router = express.Router();
 
-const guitarsRouter = express.Router();
+let guitars = [
+  {
+    brand: "Fender",
+    model: "Stratocaster",
+    color: "Maple",
+    id: shortid.generate(),
+  },
+  {
+    brand: "Gibson",
+    model: "Les Paul",
+    color: "Sunburst",
+    id: shortid.generate(),
+  },
+  {
+    brand: "Fender",
+    model: "Mustang",
+    color: "Tobacco",
+    id: shortid.generate(),
+  },
+];
 
-const guitarData = require("../data/guitars.data");
+router.get("/", (req, res) => {
+  if (guitars.length == 0) {
+    res.status(404).send(`The Guitarlist is emty... 
+    
+Try adding some Guitars :)`);
+  } else {
+    res.json(guitars);
+  }
+});
 
-// all routes in here starts with /guitars
-guitarsRouter.get("/", guitarData.getGuitars);
+router.get("/:id", (req, res) => {
+  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
+  if (!foundGuitar) {
+    res.status(404).send("Guitar with given id does not exist");
+  } else {
+    res.send(foundGuitar);
+  }
+});
 
-// Adding new Guitar and giving it a random ID-number between 1-100
-guitarsRouter.post("/", guitarData.addGuitar);
+router.post("/", (req, res) => {
+  const guitarId = shortid.generate();
+  const newGuitar = {
+    brand: req.body.brand,
+    model: req.body.model,
+    color: req.body.color,
+    id: guitarId,
+  };
 
-// Fetches guitar by id
-guitarsRouter.get("/:id", guitarData.fetchGuitar);
+  if (!req.body.brand || !req.body.model || !req.body.color) {
+    res.status(404).send(`
+Failed to add Guitar...
+Guitar needs the following attributes to be added
+    
+brand: "enter brand here",
+model: "enter model here",
+color: "enter color here",`);
+  } else {
+    guitars.push(newGuitar);
+    res.send(
+      `A ${req.body.color} ${req.body.brand} ${req.body.model} was added to the database!
 
-// Put guitar by id
-guitarsRouter.put("/:id", guitarData.putGuitar);
+ID:${guitarId}`
+    );
+  }
+});
 
-// Delete guitar by id
-guitarsRouter.delete("/:id", guitarData.deleteGuitar);
+router.put("/:id", (req, res) => {
+  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
 
-module.exports = guitarsRouter;
+  if (!foundGuitar) {
+    res.status(404).send("Failed to update guitar with given id");
+  } else {
+    foundGuitar.brand = req.body.brand;
+    foundGuitar.model = req.body.model;
+    foundGuitar.color = req.body.color;
+    res.send("Guitar was updated");
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
+
+  if (!foundGuitar) {
+    res.status(404).send("Failed to delete guitar with given id");
+  } else {
+    const guitarIndex = guitars.indexOf(foundGuitar);
+    guitars.splice(guitarIndex, 1);
+
+    res.send("Guitar was deleted");
+  }
+});
+module.exports = router;
