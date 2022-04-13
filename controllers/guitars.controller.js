4aa7a98716e763bcd2d026e6606db831f364459f
lovelanai <94110810+lovelanai@users.noreply.github.com>
2022-04-13 20:12:48 +0200

    added controllers to clean up the code

---
+++ controllers/guitars.controller.js
@@ -0,0 +1,99 @@
+const shortid = require("shortid");
+
+let guitars = [
+  {
+    brand: "Fender",
+    model: "Stratocaster",
+    color: "Maple",
+    id: shortid.generate(),
+  },
+  {
+    brand: "Gibson",
+    model: "Les Paul",
+    color: "Sunburst",
+    id: shortid.generate(),
+  },
+  {
+    brand: "Fender",
+    model: "Mustang",
+    color: "Tobacco",
+    id: shortid.generate(),
+  },
+];
+
+const getGuitars = (req, res) => {
+  if (guitars.length == 0) {
+    res.status(404).send(`The Guitarlist is emty... 
+    
+Try adding some Guitars :)`);
+  } else {
+    res.json(guitars);
+  }
+};
+exports.getGuitars = getGuitars;
+
+const getGuitarById = (req, res) => {
+  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
+  if (!foundGuitar) {
+    res.status(404).send("Guitar with given id does not exist");
+  } else {
+    res.send(foundGuitar);
+  }
+};
+exports.getGuitarById = getGuitarById;
+
+const addGuitar = (req, res) => {
+  const guitarId = shortid.generate();
+  const newGuitar = {
+    brand: req.body.brand,
+    model: req.body.model,
+    color: req.body.color,
+    id: guitarId,
+  };
+
+  if (!req.body.brand || !req.body.model || !req.body.color) {
+    res.status(404).send(`
+Failed to add Guitar...
+Guitar needs the following attributes to be added
+    
+brand: "enter brand here",
+model: "enter model here",
+color: "enter color here",`);
+  } else {
+    guitars.push(newGuitar);
+    res.send(
+      `A ${req.body.color} ${req.body.brand} ${req.body.model} was added to the database!
+
+ID:${guitarId}`
+    );
+  }
+};
+exports.addGuitar = addGuitar;
+
+const changeGuitar = (req, res) => {
+  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
+
+  if (!foundGuitar) {
+    res.status(404).send("Failed to update guitar with given id");
+  } else {
+    foundGuitar.brand = req.body.brand;
+    foundGuitar.model = req.body.model;
+    foundGuitar.color = req.body.color;
+    res.send("Guitar was updated");
+  }
+};
+exports.changeGuitar = changeGuitar;
+
+const deleteGuitar = (req, res) => {
+  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
+
+  if (!foundGuitar) {
+    res.status(404).send("Failed to delete guitar with given id");
+  } else {
+    const guitarIndex = guitars.indexOf(foundGuitar);
+    guitars.splice(guitarIndex, 1);
+
+    res.send("Guitar was deleted");
+  }
+};
+exports.deleteGuitar = deleteGuitar;
--- routers/guitars.router.js
+++ routers/guitars.router.js
@@ -1,97 +1,22 @@
 const express = require("express");
-const shortid = require("shortid");
+
 const router = express.Router();
 
-let guitars = [
-  {
-    brand: "Fender",
-    model: "Stratocaster",
-    color: "Maple",
-    id: shortid.generate(),
-  },
-  {
-    brand: "Gibson",
-    model: "Les Paul",
-    color: "Sunburst",
-    id: shortid.generate(),
-  },
-  {
-    brand: "Fender",
-    model: "Mustang",
-    color: "Tobacco",
-    id: shortid.generate(),
-  },
-];
+const guitarController = require("../controllers/guitars.controller");
 
-router.get("/", (req, res) => {
-  if (guitars.length == 0) {
-    res.status(404).send(`The Guitarlist is emty... 
-    
-Try adding some Guitars :)`);
-  } else {
-    res.json(guitars);
-  }
-});
+// Gets All guitars
+router.get("/", guitarController.getGuitars);
 
-router.get("/:id", (req, res) => {
-  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
-  if (!foundGuitar) {
-    res.status(404).send("Guitar with given id does not exist");
-  } else {
-    res.send(foundGuitar);
-  }
-});
+// Gets Guitar by ID
+router.get("/:id", guitarController.getGuitarById);
 
-router.post("/", (req, res) => {
-  const guitarId = shortid.generate();
-  const newGuitar = {
-    brand: req.body.brand,
-    model: req.body.model,
-    color: req.body.color,
-    id: guitarId,
-  };
+// Adding new Guitar
+router.post("/", guitarController.addGuitar);
 
-  if (!req.body.brand || !req.body.model || !req.body.color) {
-    res.status(404).send(`
-Failed to add Guitar...
-Guitar needs the following attributes to be added
-    
-brand: "enter brand here",
-model: "enter model here",
-color: "enter color here",`);
-  } else {
-    guitars.push(newGuitar);
-    res.send(
-      `A ${req.body.color} ${req.body.brand} ${req.body.model} was added to the database!
+// Changes Guitar by ID
+router.put("/:id", guitarController.changeGuitar);
 
-ID:${guitarId}`
-    );
-  }
-});
+// Deletes Guitar by ID
+router.delete("/:id", guitarController.deleteGuitar);
 
-router.put("/:id", (req, res) => {
-  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
-
-  if (!foundGuitar) {
-    res.status(404).send("Failed to update guitar with given id");
-  } else {
-    foundGuitar.brand = req.body.brand;
-    foundGuitar.model = req.body.model;
-    foundGuitar.color = req.body.color;
-    res.send("Guitar was updated");
-  }
-});
-
-router.delete("/:id", (req, res) => {
-  const foundGuitar = guitars.find((guitar) => guitar.id === req.params.id);
-
-  if (!foundGuitar) {
-    res.status(404).send("Failed to delete guitar with given id");
-  } else {
-    const guitarIndex = guitars.indexOf(foundGuitar);
-    guitars.splice(guitarIndex, 1);
-
-    res.send("Guitar was deleted");
-  }
-});
 module.exports = router;
